a831d3fdf7dc370a05c541876131f5de3b181cd3
RishikSarkar <rishiksarkar123@gmail.com>
2023-03-15 01:41:37 -0400

    deadcode progress

--- DeadCodeElimination.c
+++ DeadCodeElimination.c
@@ -21,18 +21,106 @@ Instruction *dead_code_elimination(Instruction *head)
 		
 		Instruction *current = head->next;
 
-		int *reg = NULL;
-		reg = (int *) malloc(sizeof(int));
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
 
+		while (current->opcode != OUTPUTAI)
+		{
+			if (current != NULL && current->prev != NULL)
+			{
+				Instruction *temp = current;
+				current = current->prev;
+				current->next = temp->next;
+				//free(temp);
+				continue;
+			}
+		}
+		
 		int regArray[4096] = {0};
-		int *index;
+
+		while (current != NULL)
+		{
+			current->critical = 0;
+			
+			if (current->opcode == OUTPUTAI)
+			{
+				current->critical = 1;
+				regArray[current->field1] = 1;
+			}
+			else if (current->opcode == LOADI)
+			{
+				if (regArray[current->field2] == 1)
+				{
+					current->critical = 1;
+				}
+			}
+			else if (current->opcode == STOREAI)
+			{
+				if (regArray[current->field2] == 1)
+				{
+					current->critical = 1;
+					regArray[current->field1] = 1;
+				}
+			}
+			else
+			{
+				if (regArray[current->field3] == 1)
+				{
+					current->critical = 1;
+
+					if (current->opcode == LOADAI || current->opcode == LSHIFTI || current->opcode == RSHIFTI)
+					{
+						regArray[current->field1] = 1;
+					}
+					else
+					{
+						regArray[current->field1] = 1;
+						regArray[current->field2] = 1;
+					}
+				}
+			}
+
+			current = current->prev;
+		}
+
+		current = head->next;
+
+		while (current != NULL)
+		{
+			if (current->critical != 1)
+			{
+				if (current != NULL && current->prev != NULL)
+				{
+					Instruction *temp = current;
+					current = current->prev;
+					current->next = temp->next;
+					//free(temp);
+					continue;
+				}
+			}
+
+			current = current->next;
+		}
+
+
+		/*
+		// Begin on the instruction after: loadI 1024 => r0
+		Instruction *current = head->next;
+
+		int reg;
+
+		//int regArray[4096] = {0};
+		//int *index;
+		int index;
 
 		while (current != NULL)
 		{
 			// Case where instruction is LOADI or STOREAI: Thus, stored register is in field2
 			if (current->opcode == LOADI || current->opcode == STOREAI)
 			{
-				*reg = current->field2;
+				reg = current->field2;
 			}
 			// Case where instruction is OUTPUTAI: Thus, no register is stored
 			else if (current->opcode == OUTPUTAI)
@@ -43,59 +131,73 @@ Instruction *dead_code_elimination(Instruction *head)
 			// Case where instruction is anything else: Thus, stored register is in field3
 			else
 			{
-				*reg = current->field3;
+				reg = current->field3;
 			}
 
-			index = &regArray[*reg];
+			//index = &regArray[reg];
+			index = 0;
 
 			// Case where register is not guaranteed to be critical, so check all subsequent instructions
-			if (index == 0)
-			{
-				Instruction *nextIterator = current->next;
+			//if (*index == 0)
+			//{
+			Instruction *nextIterator = current->next;
 
-				while (nextIterator != NULL)
+			while (nextIterator != NULL)
+			{
+				// Case where instruction is LOADI: Thus, register in current is not applicable
+				if (nextIterator->opcode == LOADI)
 				{
-					// Case where instruction is LOADI: Thus, register in current is not applicable
-					if (nextIterator->opcode == LOADI)
-					{
-						nextIterator = nextIterator->next;
-						continue;
-					}
-					// Case where instruction is LOADAI, STOREAI, LSHIFTI, RSHIFTI, or OUTPUTAI: Thus, register is field1
-					else if (nextIterator->opcode == LOADAI || nextIterator->opcode == STOREAI || nextIterator->opcode == LSHIFTI || nextIterator->opcode == RSHIFTI || nextIterator->opcode == OUTPUTAI)
-					{
-						if (*reg == nextIterator->field1)
-						{
-							// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
-							*index = 1;
-							break;
-						}
-					}
-					// Case where instruction is ADD, SUB, MUL, or DIV: Thus, register can be field1 or field 2
-					else
-					{
-						if (*reg == nextIterator->field1 || *reg == nextIterator->field2)
-						{
-							// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
-							*index = 1;
-							break;
-						}
-					}
-					
 					nextIterator = nextIterator->next;
+					continue;
 				}
+				// Case where instruction is LOADAI, STOREAI, LSHIFTI, RSHIFTI, or OUTPUTAI: Thus, register is field1
+				else if (nextIterator->opcode == LOADAI || nextIterator->opcode == STOREAI || nextIterator->opcode == LSHIFTI || nextIterator->opcode == RSHIFTI || nextIterator->opcode == OUTPUTAI)
+				{
+					if (reg == nextIterator->field1)
+					{
+						// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
+						//index = 1;
+						index = 1;
+						break;
+					}
+				}
+				// Case where instruction is ADD, SUB, MUL, or DIV: Thus, register can be field1 or field 2
+				else
+				{
+					if (reg == nextIterator->field1 || reg == nextIterator->field2)
+					{
+						// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
+						//index = 1;
+						index = 1;
+						break;
+					}
+				}
+				
+				nextIterator = nextIterator->next;
 			}
+			//}
 
 			// If register is not critical, remove instruction
 			if (index == 0)
 			{
-				Instruction *temp = current;
-				current->prev->next = current->next;
-				free(temp);
+				//Instruction *temp = current;
+				//current->prev->next = current->next;
+				//free(temp);
+
+				if (current != NULL && current->prev != NULL)
+				{
+					Instruction *temp = current;
+					current = current->prev;
+					current->next = temp->next;
+					//free(temp);
+					continue;
+				}
 			}
 
 			current = current->next;
 		}
+		*/
+		
 
  		/* STUDENTS - END */
 
