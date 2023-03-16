cccb13129cf2eac7362676815a723cb6628ed733
RishikSarkar <rishiksarkar123@gmail.com>
2023-03-15 20:46:41 -0400

    deadcode without comments

--- DeadCodeElimination.c
+++ DeadCodeElimination.c
@@ -39,8 +39,9 @@ Instruction *dead_code_elimination(Instruction *head)
 		}
 		
 		int regArray[4096] = {0};
+		int offsetArray[10] = {0};
 
-		while (current != NULL)
+		while (current != NULL && current != head)
 		{
 			current->critical = 0;
 			
@@ -48,6 +49,7 @@ Instruction *dead_code_elimination(Instruction *head)
 			{
 				current->critical = 1;
 				regArray[current->field1] = 1;
+				offsetArray[(current->field2) / 4] = 1;
 			}
 			else if (current->opcode == LOADI)
 			{
@@ -58,10 +60,20 @@ Instruction *dead_code_elimination(Instruction *head)
 			}
 			else if (current->opcode == STOREAI)
 			{
-				if (regArray[current->field2] == 1)
+				if (regArray[current->field2] == 1 && offsetArray[(current->field3) / 4] == 1)
 				{
 					current->critical = 1;
 					regArray[current->field1] = 1;
+					offsetArray[(current->field3) / 4] = 0;
+				}
+			}
+			else if (current->opcode == LOADAI)
+			{
+				if (regArray[current->field3] == 1)
+				{
+					current->critical = 1;
+					regArray[current->field1] = 1;
+					offsetArray[(current->field2) / 4] = 1;
 				}
 			}
 			else
@@ -70,7 +82,7 @@ Instruction *dead_code_elimination(Instruction *head)
 				{
 					current->critical = 1;
 
-					if (current->opcode == LOADAI || current->opcode == LSHIFTI || current->opcode == RSHIFTI)
+					if (current->opcode == LSHIFTI || current->opcode == RSHIFTI)
 					{
 						regArray[current->field1] = 1;
 					}
@@ -89,7 +101,7 @@ Instruction *dead_code_elimination(Instruction *head)
 
 		while (current != NULL)
 		{
-			if (current->critical != 1)
+			if (current->critical == 0)
 			{
 				if (current != NULL && current->prev != NULL)
 				{
@@ -102,102 +114,7 @@ Instruction *dead_code_elimination(Instruction *head)
 			}
 
 			current = current->next;
-		}
-
-
-		/*
-		// Begin on the instruction after: loadI 1024 => r0
-		Instruction *current = head->next;
-
-		int reg;
-
-		//int regArray[4096] = {0};
-		//int *index;
-		int index;
-
-		while (current != NULL)
-		{
-			// Case where instruction is LOADI or STOREAI: Thus, stored register is in field2
-			if (current->opcode == LOADI || current->opcode == STOREAI)
-			{
-				reg = current->field2;
-			}
-			// Case where instruction is OUTPUTAI: Thus, no register is stored
-			else if (current->opcode == OUTPUTAI)
-			{
-				current = current->next;
-				continue;
-			}
-			// Case where instruction is anything else: Thus, stored register is in field3
-			else
-			{
-				reg = current->field3;
-			}
-
-			//index = &regArray[reg];
-			index = 0;
-
-			// Case where register is not guaranteed to be critical, so check all subsequent instructions
-			//if (*index == 0)
-			//{
-			Instruction *nextIterator = current->next;
-
-			while (nextIterator != NULL)
-			{
-				// Case where instruction is LOADI: Thus, register in current is not applicable
-				if (nextIterator->opcode == LOADI)
-				{
-					nextIterator = nextIterator->next;
-					continue;
-				}
-				// Case where instruction is LOADAI, STOREAI, LSHIFTI, RSHIFTI, or OUTPUTAI: Thus, register is field1
-				else if (nextIterator->opcode == LOADAI || nextIterator->opcode == STOREAI || nextIterator->opcode == LSHIFTI || nextIterator->opcode == RSHIFTI || nextIterator->opcode == OUTPUTAI)
-				{
-					if (reg == nextIterator->field1)
-					{
-						// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
-						//index = 1;
-						index = 1;
-						break;
-					}
-				}
-				// Case where instruction is ADD, SUB, MUL, or DIV: Thus, register can be field1 or field 2
-				else
-				{
-					if (reg == nextIterator->field1 || reg == nextIterator->field2)
-					{
-						// Change regArray[*reg] from 0 to 1, since register is guaranteed to be critical
-						//index = 1;
-						index = 1;
-						break;
-					}
-				}
-				
-				nextIterator = nextIterator->next;
-			}
-			//}
-
-			// If register is not critical, remove instruction
-			if (index == 0)
-			{
-				//Instruction *temp = current;
-				//current->prev->next = current->next;
-				//free(temp);
-
-				if (current != NULL && current->prev != NULL)
-				{
-					Instruction *temp = current;
-					current = current->prev;
-					current->next = temp->next;
-					//free(temp);
-					continue;
-				}
-			}
-
-			current = current->next;
-		}
-		*/
-		
+		}		
 
  		/* STUDENTS - END */
 
