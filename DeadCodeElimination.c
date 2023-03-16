2b61bc99405a9709a449319a8808ffb5f2212c22
RishikSarkar <rishiksarkar123@gmail.com>
2023-03-15 23:41:49 -0400

    deadcode

--- DeadCodeElimination.c
+++ DeadCodeElimination.c
@@ -19,77 +19,73 @@ Instruction *dead_code_elimination(Instruction *head)
 
 		/* STUDENTS - BEGIN */
 		
-		Instruction *current = head->next;
+		// Set current as the head of the list
+		Instruction *current = head;
 
+		// Iterate to the end of the linked list: current is now the last instruction
 		while (current->next != NULL)
 		{
 			current = current->next;
 		}
-
-		while (current->opcode != OUTPUTAI)
-		{
-			if (current != NULL && current->prev != NULL)
-			{
-				Instruction *temp = current;
-				current = current->prev;
-				current->next = temp->next;
-				//free(temp);
-				continue;
-			}
-		}
 		
-		int regArray[4096] = {0};
-		int offsetArray[10] = {0};
+		int regArray[4096] = {0};	// Boolean array of registers (default value: 0)
+		int offsetArray[10] = {0};	// Boolean array of offset values for r0 (default value: 0)
 
+		// Iterate backwards from the last instruction
 		while (current != NULL && current != head)
 		{
-			current->critical = 0;
+			current->critical = 0;		// The instruction is initially marked non-critical (just in case default is not 0)
 			
+			// Case where instruction is OUTPUTAI: Thus, inherently critical
 			if (current->opcode == OUTPUTAI)
 			{
-				current->critical = 1;
-				regArray[current->field1] = 1;
-				offsetArray[(current->field2) / 4] = 1;
+				current->critical = 1;							// Set instruction as critical
+				regArray[current->field1] = 1;					// Set value at the index of the register as 1
+				offsetArray[(current->field2) / 4] = 1;			// Set value at the index of the offset/4 as 1
 			}
+			// Case where instruction is LOADI: Only critical if register at field2 is critical
 			else if (current->opcode == LOADI)
 			{
 				if (regArray[current->field2] == 1)
 				{
-					current->critical = 1;
+					current->critical = 1;						// Set instruction as critical
 				}
 			}
+			// Case where instruction is STOREAI: Only critical if register at field2 and the offset are critical
 			else if (current->opcode == STOREAI)
 			{
 				if (regArray[current->field2] == 1 && offsetArray[(current->field3) / 4] == 1)
 				{
-					current->critical = 1;
-					regArray[current->field1] = 1;
-					offsetArray[(current->field3) / 4] = 0;
+					current->critical = 1;						// Set instruction as critical
+					regArray[current->field1] = 1;				// Set value at the index of the field1 register as 1
+					offsetArray[(current->field3) / 4] = 0;		// Set value at the index of the offset/4 back to 0
 				}
 			}
+			// Case where instruction is LOADAI: Only critical if register at field3 is critical
 			else if (current->opcode == LOADAI)
 			{
 				if (regArray[current->field3] == 1)
 				{
-					current->critical = 1;
-					regArray[current->field1] = 1;
-					offsetArray[(current->field2) / 4] = 1;
+					current->critical = 1;						// Set instruction as critical
+					regArray[current->field1] = 1;				// Set value at the index of the field1 register as 1
+					offsetArray[(current->field2) / 4] = 1;		// Set value at the index of the offset/4 as 1
 				}
 			}
+			// Case where instruction is LSHIFTI, RSHIFTI, ADD, SUB, MUL, or DIV: Only critical if register at field3 is critical
 			else
 			{
 				if (regArray[current->field3] == 1)
 				{
-					current->critical = 1;
+					current->critical = 1;						// Set instruction as critical
 
 					if (current->opcode == LSHIFTI || current->opcode == RSHIFTI)
 					{
-						regArray[current->field1] = 1;
+						regArray[current->field1] = 1;			// Set only value at the index of the field1 register as 1
 					}
 					else
 					{
-						regArray[current->field1] = 1;
-						regArray[current->field2] = 1;
+						regArray[current->field1] = 1;			// Set value at the index of the field1 register as 1
+						regArray[current->field2] = 1;			// Set value at the index of the field2 register as 1
 					}
 				}
 			}
@@ -97,20 +93,21 @@ Instruction *dead_code_elimination(Instruction *head)
 			current = current->prev;
 		}
 
+		// Set current to the instruction after "loadI 1024 => r0"
 		current = head->next;
 
 		while (current != NULL)
-		{
-			if (current->critical == 0)
+		{	
+			// If instruction is not critical, remove from linked list
+			if (current->critical == 0 && current != NULL && current->prev != NULL)
 			{
-				if (current != NULL && current->prev != NULL)
-				{
-					Instruction *temp = current;
-					current = current->prev;
-					current->next = temp->next;
-					//free(temp);
-					continue;
-				}
+				Instruction *temp = current;
+				temp = temp->prev;
+				temp->next = current->next;			// Make the previous node point to the node after current
+				current->next->prev = temp;			// Make the next node point to the node before current
+				free(current);						// Free the current node
+				current = temp;						// Set the previous node as the new current node
+				continue;
 			}
 
 			current = current->next;
